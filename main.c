bf09cecbc55b0c900c51f3c9624e2b62e78518cb
shhhQuiettt <krzysztof.skro@gmail.com>
2023-04-08 17:48:18 +0200

    Add error paths

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 FILE *open_file(char *filename) {
   FILE *fp;
@@ -12,10 +13,16 @@ FILE *open_file(char *filename) {
   return fp;
 }
 
-// To implement
-short is_valid_section(char *section) { return 1; }
+short is_proper_cher(char c) { return isalnum(c) || c == '-'; }
 
-short is_valid_key(char *section) { return 1; }
+short is_valid_identifier(char *identifier) {
+  for (int i = 0; i < strlen(identifier); i++) {
+    if (!is_proper_cher(identifier[i])) {
+      return 0;
+    }
+  }
+  return 1;
+}
 
 short is_section(char *line) {
   return (line[0] == '[' && line[strlen(line) - 2] == ']') ? 1 : 0;
@@ -25,7 +32,7 @@ short is_section(char *line) {
 //          line:        section_container:
 // Example: [abc]\n  ->  abc
 // Don't ask me how it works
-void *extract_section(char *line_buff, char *section_container) {
+void extract_section(char *line_buff, char *section_container) {
   strncpy(section_container, line_buff + 1, strlen(line_buff) - 3);
   section_container[strlen(line_buff) - 3] = '\0';
 }
@@ -35,7 +42,7 @@ int main() {
   char *filename = "example.ini";
   // looking for
   char section[31] = "slippery-balance";
-  char key[31] = "buttery-profile";
+  char key[31] = "buttery-profileee";
 
   char current_section[31];
   char current_key[31];
@@ -54,20 +61,18 @@ int main() {
       extract_section(line_buff, current_section);
 
       if (strcmp(current_section, section) == 0) {
-        printf("section found: %s\n", current_section);
-
-        // looking for key
-        // TODO: what when key is not in section?
-        while (fscanf(fp, "%s = %s\n", current_key, current_value) != EOF) {
+        while (fscanf(fp, "%s = %s\n", current_key, current_value) > 0) {
           if (strcmp(current_key, key) == 0) {
-            printf("key found: %s\n", current_key);
-            printf("value: %s\n", current_value);
-
+            printf("Value: %s\n", current_value);
+            return 0;
           }
         }
+        printf("Failed to find key %s in section [%s]", key, section);
+        return 1;
       }
     }
   }
+  printf("Failed to find section [%s]", section);
   fclose(fp);
-  return 0;
+  return 1;
 }
